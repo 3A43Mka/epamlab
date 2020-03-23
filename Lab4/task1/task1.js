4822a772e9be4ac5f70622656ed702965f5540bd
3A43Mka <3a43mka@gmail.com>
2020-03-23 20:06:01 +0200

    Task1 done

--- Lab4/task1/task1.js
+++ Lab4/task1/task1.js
@@ -6,16 +6,46 @@ let borderColor = document.getElementById("border-color");
 let imageAlt = document.getElementById("image-alt");
 let mainImage = document.getElementById("main-image");
 
-function Logger() {
-    console.log(imageWidth.value);
-    console.log(imageHeight.value);
-    console.log(borderWidth.value);
-    console.log(borderColor.value);
-    console.log(imageAlt.value);
-    console.log(mainImage);
-    mainImage.style.width = imageWidth.value + 'px';
-    mainImage.style.height = imageHeight.value + 'px';
+let options = [imageWidth, imageHeight, borderWidth, borderColor, imageAlt];
 
+function Logger() {
+    let flag = false;
+    options.forEach(option => {
+        !option.value ? (option.style.border = "2px solid red") && (flag = true) : option.style.border = "";
+    });
+    if (/\D/.test(imageWidth.value)) {
+        imageWidth.style.border = "2px solid red";
+        flag = true
+    } else {
+        imageWidth.style.border = "";
+    }
+    if (/\D/.test(imageHeight.value)) {
+        imageHeight.style.border = "2px solid red";
+        flag = true
+    } else {
+        imageHeight.style.border = "";
+    }
+    if (/\D/.test(borderWidth.value)) {
+        borderWidth.style.border = "2px solid red";
+        flag = true
+    } else {
+        borderWidth.style.border = "";
+    }
+    var latin = /^[0-9a-zA-Z ]+$/;
+    if (!imageAlt.value.match(latin)) {
+        imageAlt.style.border = "2px solid red";
+        flag = true
+    } else {
+        imageAlt.style.border = "";
+    }
+
+    if (!flag) {
+        mainImage.style.width = imageWidth.value + 'px';
+        mainImage.style.height = imageHeight.value + 'px';
+        mainImage.setAttribute('alt', imageAlt.value);
+        mainImage.style.borderColor = borderColor.value;
+        mainImage.style.borderWidth = borderWidth.value + 'px';
+    }
 }
 
 submit.addEventListener("click", Logger);
