3075bb51e49804ad87e3ba39aa90f48d60dfaad5
3A43Mka <3a43mka@gmail.com>
2020-03-25 13:21:08 +0200

    Task2 diagram fixed

--- Lab4/task2/task2.js
+++ Lab4/task2/task2.js
@@ -2,17 +2,15 @@ let mainTable = document.getElementById("main-table");
 let addButton = document.getElementById("button-add");
 let diagram = document.getElementById("diagram");
 
-let colors = ["red", "blue", "black", "yellow", "green", "DEEPPINK", "orange", "BLUEVIOLET", "LIME", "AQUA", "BROWN"];
+let colors = ["CRIMSON", "blue", "TOMATO", "green", "DEEPPINK", "orange", "BLUEVIOLET", "LIME", "AQUA", "BROWN"];
 
 function AddNew() {
     let newRow = document.createElement('tr');
     let deleteAction = document.createElement('td');
     deleteAction.innerText = "Видалити";
-    deleteAction.addEventListener("click",
-        function() {
-            RemoveRow(this);
-        }
-    );
+    deleteAction.style.cursor = "pointer";
+    deleteAction.style.color = "#0097c5";
+    deleteAction.addEventListener("click", function() { RemoveRow(this); });
     newRow.appendChild(deleteAction);
     let groupName = document.createElement('td');
     groupName.innerText = "КП-ХХ";
@@ -33,6 +31,11 @@ function ReadTable() {
     for (let i = 1; i < mainTable.rows.length; i++) {
         let group = {};
         group.title = mainTable.rows[i].cells[1].innerText;
+        if (/\D/.test(mainTable.rows[i].cells[2].innerText)) {
+            mainTable.rows[i].cells[2].style.border = '2px solid red'
+        } else {
+            mainTable.rows[i].cells[2].style.border = '';
+        }
         group.num = mainTable.rows[i].cells[2].innerText;
         groups.push(group);
     }
@@ -57,18 +60,25 @@ function DrawDiagrams() {
     for (let i = 0; i < data.length; i++) {
         currHeight = data[i].num * coef;
         // (currHeight == 0) ? currHeight = 1 * coef: null;
+        (currHeight < 25) ? (currHeight = 25) : null;
 
         let column = document.createElement("div");
         column.style.background = colors[i % colors.length];
         column.style.width = columnWidth + 'px';
         column.style.height = currHeight + 'px';
+        column.style.lineHeight = currHeight - 10 + 'px';
+        column.style.color = 'white';
         column.style.marginLeft = (+columnGap + (i * columnGap) + (i * columnWidth)) + 'px';
         column.style.position = "absolute";
         column.style.bottom = '40px';
+        column.style.textAlign = "center";
         column.setAttribute("class", "diagram");
+        column.setAttribute("data-num", data[i].num);
+        column.addEventListener("mouseover", function() { ShowInfo(this); });
+        column.addEventListener("mouseout", function() { HideInfo(this); });
+
         let caption = document.createElement("div");
         caption.style.width = columnWidth + 'px';
-        // caption.style.height = '25px';
         caption.style.textAlign = "center";
         caption.innerText = data[i].title;
         caption.style.position = "absolute";
@@ -85,5 +95,16 @@ function RemoveRow(btn) {
     row.remove();
     DrawDiagrams();
 }
+
+function ShowInfo(diagram) {
+    diagram.innerText = diagram.getAttribute("data-num");
+    diagram.style.border = '5px double white';
+}
+
+function HideInfo(diagram) {
+    diagram.innerText = '';
+    diagram.style.border = '';
+}
+
 
 addButton.addEventListener("click", AddNew);
